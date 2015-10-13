56956b00e2fd23a7c1000793da66354ecf5c1b83
DustJ <dustin@dustinjacobson.com>
2015-10-12 20:22:18 -0600

    Angular

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name dustApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the dustApp
+ */
+angular.module('dustApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name dustApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the dustApp
+ */
+angular.module('dustApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
